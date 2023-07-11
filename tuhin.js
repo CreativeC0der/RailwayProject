8b8c96f251330d2235d5b9971b6185f19e54d744
tuhin <tuhin301201@gmail.com>
2023-07-11 14:22:33 +0530

    new push

---
+++ tuhin.js
@@ -0,0 +1,51 @@
+// get the client
+
+const express = require('express')
+const mysql = require('mysql2');
+const hbs = require('express-handlebars')
+
+const app = express();
+
+// create the connection to database
+
+app.engine('handlebars', hbs.engine());
+// initialize views and Handlebars
+app.set('view engine', 'handlebars')
+app.set('views', './Views')
+
+// simple query
+
+//login and sign up
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req, res) => {
+    res.render('login', {})
+})
+app.get('/signup', (req, res) => {
+    res.render('signup', {})
+})
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard', {})})
+
+
+// menu (home, contact, about)
+
+app.get('/about',(req,res)=>
+{
+    res.render('about',{})
+})
+app.get('/contact',(req,res)=>
+{
+    res.render('contact',{})
+})
+app.get('/home',(req,res)=>
+{
+    res.render('login',{})
+})
+
+
+// for image
+app.use(express.static('Views'));
+app.use('/images', express.static('images'));
+
+
+app.listen(3000, 'localhost');
