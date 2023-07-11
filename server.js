7e01102b2d769836d3a112c0e799d636fb43a661
shrijon <shrijon610@gmail.com>
2023-07-11 21:00:13 +0530

    Created Routes folder. Added dashboard, about,contact. Added ER

--- server.js
+++ server.js
@@ -1,53 +1,35 @@
 // get the client
 const express = require('express')
-const mysql = require('mysql2');
 const hbs = require('express-handlebars')
 
-
+//import Routes
+const aboutRoute = require('./Routes/about');
+const contactRoute = require('./Routes/contact');
+const dashboardRoute = require('./Routes/dashboard');
 
 const app = express();
 
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'railway'
-});
-
-connection.connect((err) => {
-    if (err)
-        console.log(err)
-    else
-        console.log("Connected to DB")
-})
-
-app.engine('handlebars', hbs.engine());
 // initialize views and Handlebars
+app.engine('handlebars', hbs.engine());
 app.set('view engine', 'handlebars')
 app.set('views', './Views')
-
-// simple query
-
 app.use(express.urlencoded({ extended: true }))
 
+
+//Routing
+
 app.get('/', (req, res) => {
-    res.render('homepage', {})
+    res.render('login', {})
 })
 
-app.post('/', (req, res) => {
-    query = `select * 
-    from booking NATURAL JOIN passenger
-    where name='${req.body.name}'`
+app.get('/signup', (req, res) => {
+    res.render('signup', {})
+})
 
+app.get('/about', aboutRoute);
 
-    connection.query(query, (err, results, fields) => {
-        if (err)
-            console.log('ERRORRRR');
+app.get('/contact', contactRoute);
 
-        res.render('bookings', { details: results })
-    });
-});
+app.post('/dashboard', dashboardRoute);
 
-app.
-
-    app.listen(3000, 'localhost');
+app.listen(3000, 'localhost');
