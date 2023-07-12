1b4216356b7d30253d367218b8cc140618477d20
shrijon <shrijon610@gmail.com>
2023-07-12 20:42:15 +0530

    Added Dashboard Code

--- Routes/dashboard.js
+++ Routes/dashboard.js
@@ -18,17 +18,40 @@ connection.connect((err) => {
 
 app.post('/dashboard', (req, res) => {
     console.log(req.body);
-    query = `select * 
-    from booking NATURAL JOIN passenger
-    where acc_num='${req.body.accnum}' AND email='${req.body.email}'`
 
+    var responseData = {};
+    query = `select name,email,acc_num,phone,address 
+    from passenger where 
+    acc_num="${req.body.accnum}" AND email="${req.body.email}"`;
     connection.query(query, (err, results, fields) => {
         if (err)
-            console.log('ERRORRRR');
-
+            console.log(err);
         console.log(results);
-        res.render('dashboard', { details: results })
-    });
+        responseData = results[0];
+        query = `select booking_id,train_number,journey_date,fare
+        from booking NATURAL JOIN passenger
+        where acc_num='${req.body.accnum}' AND email='${req.body.email}'`
+
+        connection.query(query, (err, results, fields) => {
+            if (err)
+                console.log('ERRORRRR');
+
+            console.log(results);
+            responseData.details = results;
+
+            query = `select * from station`;
+            connection.query(query, (err, results, fields) => {
+                console.log(results);
+
+                responseData.stations = results;
+                console.log(`response=`);
+                console.log(responseData);
+                res.render('dashboard', responseData);
+            });
+
+        });
+    })
+
 });
 
 module.exports = app;
